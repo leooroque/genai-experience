f80434e846d33f5e00393b3d152b4db0c0b78ec1
Leonardo Roque <leo.o.roquee@gmail.com>
2024-10-01 14:33:39 -0300

    new structure genai-experience

--- api/factory/getEnvironmentVariables.js
+++
@@ -1,9 +0,0 @@
- 
-
-module.exports = getEnvironmentVariables = () =>{
-    getEnvironmentVariables.getVariable = (variable) => {
-        return process.env[variable];
-    }
-
-    return getEnvironmentVariables;
-}
--- index.js
+++
@@ -1,5 +0,0 @@
-var application = require('./config/express')();
-
-application.listen(3000, function(){
-    console.log('Servidor rodando!');
-})
